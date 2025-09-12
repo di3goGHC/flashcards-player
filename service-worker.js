6666fcc2eb9b54cc275f81217757de8352ec36ce
di3goGHC <di3go.jcr@gmail.com>
2025-09-12 22:14:57 +0200

    Correccion

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
- HEAD
 const CACHE_NAME = 'flashcards-cache-v1';
 const urlsToCache = [
   '/',
@@ -52,59 +51,4 @@ self.addEventListener('activate', event => {
       );
     })
   );
-
-const CACHE_NAME = 'flashcards-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/manifest.json',
-  '/favicon.ico',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
- c464b9714b22c9feba4d92f4ec46068aa96ab077
 });
